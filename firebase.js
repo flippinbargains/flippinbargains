d1167bf854f945e1ef38a23707222b97093eb843
flippinbargains <j.r_leather_works@yahoo.com>
2025-07-16 16:41:30 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,8 @@
 // firebase.js
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCn2yB_oVrb-st1uosqN8YbjdzGp2EF--o",
-  authDomain: "flippinbargains-e8a8a.firebaseapp.com",
-  projectId: "flippinbargains-e8a8a",
-  storageBucket: "flippinbargains-e8a8a.appspot.com",
-  messagingSenderId: "920267203424",
-  appId: "1:920267203424:web:d703d051131d8e2d9bb405",
-  measurementId: "G-03GDHRHYYC"
-};
+export const firebaseConfig = { apiKey: "AIzaSyCn2yB_oVrb-st1uosqN8YbjdzGp2EF--o", authDomain: "flippinbargains-e8a8a.firebaseapp.com", projectId: "flippinbargains-e8a8a", storageBucket: "flippinbargains-e8a8a.appspot.com", messagingSenderId: "920267203424", appId: "1:920267203424:web:d703d051131d8e2d9bb405", measurementId: "G-03GDHRHYYC" };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+
