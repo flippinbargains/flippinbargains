efc8dc246358117e36b8428045d6c7bd6907b218
flippinbargains <j.r_leather_works@yahoo.com>
2025-07-17 20:10:20 -0400

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-// firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-
-export const firebaseConfig = { apiKey: "AIzaSyCn2yB_oVrb-st1uosqN8YbjdzGp2EF--o", authDomain: "flippinbargains-e8a8a.firebaseapp.com", projectId: "flippinbargains-e8a8a", storageBucket: "flippinbargains-e8a8a.appspot.com", messagingSenderId: "920267203424", appId: "1:920267203424:web:d703d051131d8e2d9bb405", measurementId: "G-03GDHRHYYC" };
-
-export const app = initializeApp(firebaseConfig);
-
